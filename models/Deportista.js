8ed6890737d662270e131d71dd505cccf44167e1
Bruno <brunopaz00@yahoo.com.mx>
2022-09-26 12:50:32 -0500

    Campos agregados a deportista

--- models/Deportista.js
+++ models/Deportista.js
@@ -8,19 +8,26 @@ const Deportista = db.define('deportista', {
         autoIncrement: true
     },
     expediente: {type: Sequelize.STRING(32),allowNull: false},
-    nombreCompleto: {type: Sequelize.STRING(100),allowNull: false},
-    correo:{type: Sequelize.STRING(80), allowNull:false},
-    telefono: {type: Sequelize.STRING(32), allowNull:false},
-    telefonoEmergencia: {type: Sequelize.STRING(32), allowNull:false},
-    numSeguroSocial: {type: Sequelize.STRING(100), allowNull:false},
-    genero: {
+    nombres: {type: Sequelize.STRING(100),allowNull: false},
+    apellidos: {type: Sequelize.STRING(100),allowNull: false},
+    sexo: {
         // 0: hombre, 1: mujer
         type: Sequelize.INTEGER(1), 
         allowNull:false
     },
+    facultad:{type: Sequelize.STRING(80), allowNull:false},
+    jugadorSeleccionado: {
+        // 0: si, 1: no
+        type: Sequelize.INTEGER(1), 
+        allowNull:false
+    },
+    numSeguroSocial: {type: Sequelize.STRING(100), allowNull:false},
+    correo:{type: Sequelize.STRING(80), allowNull:false},
+    telefono: {type: Sequelize.STRING(32), allowNull:false},
+    telefonoEmergencia: {type: Sequelize.STRING(32), allowNull:false},
+    fotoCardex: {type: Sequelize.STRING(64), allowNull:false},
     fotoIdentificacionOficial: {type: Sequelize.STRING(64), allowNull:false},
     foto: {type: Sequelize.STRING(64), allowNull:false},
-    fotoCardex: {type: Sequelize.STRING(64), allowNull:false},
     fechaDadoDeAlta: {type: Sequelize.DATEONLY(64), defaultValue: Sequelize.NOW},
     activo: {
         type:Sequelize.INTEGER(1),
