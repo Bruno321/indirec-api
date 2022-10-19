42305ce713361a96d4427fd9ef51ed3cf4838ea8
Daniel Aros Ramírez <danielaros200@gmail.com>
2022-10-19 15:57:44 -0500

    estructura principal del pdf

--- services/deportistasPdfService.js
+++ services/deportistasPdfService.js
@@ -7,33 +7,11 @@ async function buildPDF(dataCallback, endCallback, data) {
   doc.on('data', dataCallback);
   doc.on('end', endCallback);
 
-
-  doc.fontSize(12).font('Helvetica-Bold').text("CONSEJO NACIONAL DEL DEPORTE DE LA EDUCACIÓN, A.C:", 74, 240)
-
-  //fecha?
-  doc.fontSize(12).font('Helvetica-Bold').text("UNIVERSIDAD NACIONAL 2021:", 74, 240)
-  doc.fontSize(12).font('Helvetica-Bold').text("CÉDULA DE INSCRIPCIÓN:", 74, 240)
-
-//   de donde
-  doc.fontSize(12).font('Helvetica-Bold').text("FÚTBOL ASOCIACIÓN:", 74, 240)
-
-  doc.fontSize(9).font('Helvetica').text(`Fecha de solicitud: ${data.apellidos}`, 450, 35)
-
-  doc.fontSize(25).font('Helvetica-Bold').text(data.nombres, 73, 60) // Título de vacante
-
-  doc.fontSize(12).font('Helvetica').text(data.empresa.nombreEmpresa, 74, 105) // 
-
-
-  doc.fontSize(12).font('Helvetica-Bold').text(`Carrera: ${data.numero}`, 74, 165)
-
-
-
-
-  
-  doc.fontSize(12).font('Helvetica').text(`${data.tipoContratacion}`, 74, 260, {
-    width: 135
-  })
-
+  doc.font('Helvetica-Bold').fontSize(10).fillColor('#8C2E40').text('CONSEJO NACIONAL DEL DEPORTE DE LA EDUCACIÓN, A.C.').moveDown(0.5);
+  doc.font('Helvetica-Bold').fontSize(18).fillColor('black').text('UNIVERSIADA NACIONAL 2021');
+  doc.font('Helvetica').fontSize(10).fillColor('black').text('CÉDULA DE INSCRIPCIÓN');
+  doc.moveDown();
+  doc.font('Helvetica-Bold').fontSize(18).fillColor('#8C2E40').text('FÚTBOL ASOCIACIÓN');
 
   doc.end();
 }
